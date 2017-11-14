b112d344b6f51f08a2d75fa0ca5502d988e20758
Ujjal Kumar Das <ujjaldas223@gmail.com>
2017-11-15 01:07:35 +0530

    Create example.py

---
+++ reorder/example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 01:03:18 2017
+
+@author: ujjaldas223
+"""
+
+from util import reorder
+
+print reorder('What were you doing yesterday')
+print reorder('When are you going to school')
+print reorder('I am going to school')
