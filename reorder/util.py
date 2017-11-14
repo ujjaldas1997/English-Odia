ebfad54bcbfdda7aa566f444ff698d77778b89cc
Ujjal Kumar Das <ujjaldas223@gmail.com>
2017-11-15 01:08:37 +0530

    Add files via upload

---
+++ reorder/util.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 19:29:50 2017
+
+@author: ujjaldas223
+"""
+
+from pycorenlp import StanfordCoreNLP
+import re
+from nltk.tree import Tree
+nlp = StanfordCoreNLP('http://localhost:9000')
+
+def reorder(sentence):
+    tag_sen = nlp.annotate(sentence, properties={           #first import the parse tree
+  'annotators': 'tokenize,ssplit,pos,depparse,parse',
+  'outputFormat': 'json'
+  })
+    tag_sen = str(tag_sen['sentences'][0]['parse'])
+    sen_type = Tree.fromstring(tag_sen)[0].label()          #identify the type of sentence
+    #print 'type', sen_type
+    
+    tag_sen = re.sub('[ \t\n]+', ' ', tag_sen)              #remove any new line and redundant whitespace character
+    
+    file_name = {'S' : 'declarative.txt', 'SBARQ' : 'interrogative.txt'}
+    #print file_name[sen_type]
+    rule = open(file_name[sen_type])                        #open the respective file
+    for line in rule:                                       #reorder according to the rule
+        line = line.strip()
+        key, order = line.split('#')
+        key, order = key.strip(), order.strip()
+        tag_sen = re.sub(key, order, tag_sen)
+        #print tag_sen'''
+    
+    #print tag_sen
+    tag_sen = re.findall(r'\(\S+ \S+?\)', tag_sen)           #extract all the tags
+    #print tag_sen
+    return tag_sen
+    
+#print reorder('What were you doing yesterday')
+#print reorder('When are you going to school')
+#print reorder('I am going to school')
